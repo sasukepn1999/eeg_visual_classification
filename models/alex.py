b9099679affdac00e7d89d606845e6320fff2f20
sasukepn1999 <36978783+sasukepn1999@users.noreply.github.com>
2021-05-24 17:26:46 +0700

    Add files via upload

---
+++ models/alex.py
@@ -0,0 +1,78 @@
+import sys
+import os
+import random
+import math
+import time
+import torch; torch.utils.backcompat.broadcast_warning.enabled = True
+from torchvision import transforms, datasets
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim
+import torch.backends.cudnn as cudnn; cudnn.benchmark = True
+import numpy as np
+
+class Model(nn.Module):
+
+    def __init__(self, num_classes=40):
+        """
+        Define and allocate layers for this neural net.
+        Args:
+            num_classes (int): number of classes to predict with this model
+        """
+        super().__init__()
+        # input size should be : (b x 3 x 227 x 227)
+        # The image in the original paper states that width and height are 224 pixels, but
+        # the dimensions after first convolution layer do not lead to 55 x 55.
+        self.net = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=96, kernel_size=11, stride=1, padding=3),  # (b x 96 x 55 x 55)
+            nn.ReLU(),
+            nn.LocalResponseNorm(size=5, alpha=0.0001, beta=0.75, k=2),  # section 3.3
+            nn.MaxPool2d(kernel_size=3, stride=2),  # (b x 96 x 27 x 27)
+            nn.Conv2d(96, 256, 5, padding=2),  # (b x 256 x 27 x 27)
+            nn.ReLU(),
+            nn.LocalResponseNorm(size=5, alpha=0.0001, beta=0.75, k=2),
+            nn.MaxPool2d(kernel_size=3, stride=2),  # (b x 256 x 13 x 13)
+            nn.Conv2d(256, 384, 3, padding=1),  # (b x 384 x 13 x 13)
+            nn.ReLU(),
+            nn.Conv2d(384, 384, 3, padding=1),  # (b x 384 x 13 x 13)
+            nn.ReLU(),
+            nn.Conv2d(384, 256, 3, padding=1),  # (b x 256 x 13 x 13)
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2),  # (b x 256 x 6 x 6)
+        )
+        # classifier is just a name for linear layers
+        self.classifier = nn.Sequential(
+            nn.Dropout(p=0.5, inplace=True),
+            nn.Linear(in_features=(256 * 7 * 7), out_features=4096),
+            nn.ReLU(),
+            nn.Dropout(p=0.5, inplace=True),
+            nn.Linear(in_features=4096, out_features=2048),
+            nn.ReLU(),
+            nn.Linear(in_features=2048, out_features=num_classes),
+        )
+        self.init_bias()  # initialize bias
+        
+    def forward(self, x):
+        """
+        Pass the input through the net.
+        Args:
+            x (Tensor): input tensor
+        Returns:
+            output (Tensor): output tensor
+        """
+        x = self.net(x)
+        x = x.view(-1, 256 * 7 * 7)  # reduce the dimensions for linear layer input
+        return x, self.classifier(x)
+
+    def inference(self, x):
+        """
+        Pass the input through the net.
+        Args:
+            x (Tensor): input tensor
+        Returns:
+            output (Tensor): output tensor
+        """
+        x = self.net(x)
+        x = x.view(-1, 256 * 7 * 7)  # reduce the dimensions for linear layer input
+        return self.classifier(x)
